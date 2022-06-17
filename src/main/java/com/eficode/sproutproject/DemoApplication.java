53173f8863240ff2ded85201fc5330bbc98a7c5d
nenokkiasp <nenokkiasp@users.noreply.github.com>
2022-06-17 09:13:21 +0000

    Initialize Spring project

---
+++ src/main/java/com/eficode/sproutproject/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.eficode.sproutproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eficode/sproutproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eficode.sproutproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
